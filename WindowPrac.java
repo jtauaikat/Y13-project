2521f65b3b804dfd5573549bfb0d43163eb1819a
leo <riginle@whs.school.nz>
2023-05-05 08:56:20 +1200

    Signed-off-by: leo <riginle@whs.school.nz>

---
+++ WindowPrac.java
@@ -0,0 +1,25 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Scanner;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.awt.Cursor;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Toolkit;
+import javax.swing.JButton;   
+import java.awt.Dimension;
+import java.lang.Math;
+
+public class WindowPrac extends JFrame implements Runnable, ActionListener,MouseListener{
+    
+    JButton gridButton; 
+    
+    
+    
+    
+   
+    
+}
